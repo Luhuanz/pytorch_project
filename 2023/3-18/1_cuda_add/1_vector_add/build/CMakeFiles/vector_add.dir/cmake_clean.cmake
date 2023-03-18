f75b77676edb0c35f2b78822a97b7f0bb6a6179e
Luhuanz <huanluzz123@163.com>
2023-03-18 20:07:33 +0800

    3-18

---
+++ 2023/3-18/1_cuda_add/1_vector_add/build/CMakeFiles/vector_add.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vector_add.dir/vector_add_generated_vector_add.cu.o"
+  "vector_add.pdb"
+  "vector_add"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/vector_add.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
